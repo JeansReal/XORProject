41c8dfa50d231fbe7b9d86fbb84788951bcdb7a8
Jeans Real <jkrb7@hotmail.com>
2014-03-22 22:39:21 -0600

    Ordenando
    Ordenando el codigo (Librerias)
    creacion de Batch para actualizar el codigo!

--- Run.bat
+++ Run.bat
@@ -1,9 +1,42 @@
-@ECHO OFF
+@Echo OFF
 
-ECHO Se Actualizara Las Librerias y El codigo Automaticamente
+Title Proyecto Geometria Computacional
+
+Color FC
+
+Echo Se Actualizara Las Librerias y El codigo Automaticamente.
+Echo.
+
+Echo Por Favor Guarde sus cambios en Notepad++ de lo contrario se forzara su cierre.
+    Pause
+    TaskList | find /I "notepad++.exe"
+        if %errorlevel% == 0 (
+            TaskKill /F /IM notepad++.exe
+        )
+Echo.
+
 cd C:/
 
+Echo Copiando Drivers
+    robocopy C:\TC20\XORProject\Driver C:\TC20\BIN /E /MT:16 /NJH /NFL /NJS
+Echo.
 
+Echo Copiando Librerias
+    robocopy C:\TC20\XORProject\Librerias C:\TC20\INCLUDE /E /MT:16 /NJH /NFL /NJS
+Echo.
 
+Echo Copiando Codigo Principal
+    robocopy C:\TC20\XORProject\ C:\TC20\ Apple.C /MT:16 /NJH /NFL /NJS
+Echo.
+
+Echo Creando Configuracion de Archivos
+    if exist C:\TC20\XORProject\files.config (
+        erase C:\TC20\XORProject\files.config
+    )
+
+    dir C:\TC20\XORProject\Librerias\ >> C:\TC20\XORProject\files.config
+
+Start notepad++ %1C:\TC20\XORProject\files.config
 
 Pause
+Exit
--- readme.md
+++ readme.md
@@ -9,30 +9,45 @@ Recinto: UNI-IES
 Grupo: 2M1-CO
 
 Integrantes:
+    </br>
     Kristel Aburto Bonilla
-    <br>
+    </br>
     Rosalin Sirias Osorio
-    <br>
+    </br>
     Jeans Real
 
 Universidad Nacional de Ingenieria - IES - 2014
 
 
 Driver:
-    SVGA256.BGI
+    </br>
+    <bold>SVGA256.BGI</bold>
+    </br>
         Necesario para Inicializar el Modo Grafico XGA en Turbo C 2.0
 
 Librerias:
-    Coor:
+    </br>
+    <bold>Coor:</bold>
+    </br>
         Libreria que Contiene Coordenadas para Dibujos
-    Macro:
+    </br>
+    <bold>Macro:</bold>
+    </br>
         Libreria que Contiene Macros y Definiciones para usar el Modo Grafico
-    Modo:
+    </br>
+    <bold>Modo:</bold>
         Libreria que Contiene Funciones para Usar el Modo Grafico
 
 Tipo de Indentacion:
+    </br>
     Estilo Allman, Tab = 4 Espacios
 Convencion de Codigo:
+    </br>
     PascalCase AKA (UpperCamelCase)
+    </br>
 IDE:
     Turbo C 2.0
+
+Como Compilar:
+    </br>
+    * Correr el Batch Run.bat :)
