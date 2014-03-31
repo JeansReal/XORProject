16650b9471407ca5b3d315f8506fd5fc91c250fb
kristelitaBonilla <kristelitabonilla96@gmail.com>
2014-03-30 20:44:33 -0600

    adding design
    in this committed we add a frame  and our first landscape although its
    functionality works with "getch" during this test, after is going to
    work correctly with the established statements

---
+++ Libraries/board.h
@@ -0,0 +1,20 @@
+
+void board()
+{
+	 
+Relleno(Solido,23);
+bar(900,40,getmaxx()+1,getmaxy()+1);
+bar(0,0,getmaxx()+1,40);
+bar(0,708,getmaxx()+1,getmaxy()+1);
+Relleno(Solido,36);
+bar(0,41,50,707);
+Relleno(Solido,20);
+
+bar(50,40,900,707);
+getch();
+
+landscape1();
+
+getch();
+
+}
