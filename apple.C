227cd048778361c4e0fe6de54d7a2033e1cadab3
Jeans Real <jkrb7@hotmail.com>
2014-04-05 00:21:32 -0600

    Reforzando UX y UI
    Creando Funciones y Validaciones Para dar una mejor experiencia al
    Usuario, creando FUnciones que Animan el Marco para mejorar la
    interfaz!!

--- apple.C
+++ apple.C
@@ -1,4 +1,3 @@
-
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
@@ -14,12 +13,11 @@
 #include <StdIo.h>      /* Funciones para salida de datos>> printf() */
 #include <Conio.h>      /* Validacion de Control de Teclas>> kbhit() */
 #include <Graphics.h>   /* Para Usar El Entorno Grafico de Borland */ 
-/*#include <Dos.h>        /* Animaciones>> delay() , sleep() , sound() , nosound() */
+#include <Dos.h>        /* Animaciones>> delay() , sleep() , sound() , nosound() */
 /*#include <Alloc.h>      /* Asignacion de Memoria Dinamica>> malloc() , free() */
 #include <StdLib.h>     /* Comandos del sistema>> system() , abort() , exit() */
 #include <Math.h>       /* Funciones para Calculos de Figuras Geometricas>> DrawEllipse() */
-#include <Dos.h>        /* Funcion para Animacion>> delay() */
-
+#include <String.h>     /* Funcion Para Llenar Bloque de Memoria>> memset() */
 
 /* Libreria Personalizada */
 #include "Apple/Coor.h"       /* Coordenadas */
@@ -37,6 +35,9 @@ int huge HighLevelXGA(void);
 /* Funciones Para Mostrar las Pantallas */
 void Presentacion(void);
 void Frame(void);
+
+void DrawButton();
+
 void Landscape1(void);
 
 /* Funciones Para Figuras en Modo XOR */
@@ -47,26 +48,14 @@ void DrawTriangle(ControlEje x, ControlEje y, unsigned short Perimetro);
 void DrawRectangle(ControlEje x, ControlEje y, unsigned short Perimetro);
 void DrawEllipse(ControlEje x, ControlEje y, short xRadio, short yRadio);
 
-/* Funcion para Precision de Graficos */
-#include <Mouse.h>
-void Mouse(void)
-{
-    mver();
-    mlimit(1,1,getmaxx()+1,getmaxy()+1,1);
-
-	do {
-		gotoxy(1,1) , printf("X=%3d\nY=%3d", mxpos(1), mypos(1));
-        putpixel(mxpos(1), mypos(1), 15);
-    } while(!kbhit());
-}
-/* Macro Para Realizar "Depuracion" */
-#define Pausa   getch() , abort() ;
+void SaveStep(void);
 
 /** Cuerpo Principal **/
 void main(void)
 {
-    ControlEje x=400, y=300;
-    unsigned short largo=100;
+    Direccional Tecla = 0;
+    ControlEje x = 200, y = 100;
+    unsigned short largo = 100;
 
     InitGraph();
 
@@ -77,20 +66,87 @@ void main(void)
     do {
         DrawCursor(x, y);
 
-        switch(getch())
+        switch(Tecla = getch())
         {
-            case ARRIBA:    --y;     break;
-            case ABAJO:     ++y;     break;
-            case DERECHA:   ++x;     break;
-            case IZQUIERDA: --x;     break;
-            case MAS:       ++largo; break;
-            case MENOS:     --largo; break;
+            case ARRIBA:    y-=5;     break;
+            case ABAJO:     y+=5;     break;
+            case DERECHA:   x+=5;     break;
+            case IZQUIERDA: x-=5;     break;
+            case MAS:       largo+=5; break;
+            case MENOS:     largo-=5; break;
             case X:
                 _xorMode = (_xorMode) ? False : True ;
+            break;
+            case ENTER:
+                _figActive = True;
             break;
 
             case 97:    exit(0);
         }
 
+        /* Si el Cursor esta en la seccion de figuras, verificar en cual boton esta! */
+        if (x >= 900 && x <= getmaxx())
+            if (y >= 40 && y <= 173)
+                DrawButton(900, 40, 1020, 173, True);
+            else if (y >= 174 && y <= 308)
+                DrawButton(900, 173, 1020, 308, True);
+            else if (y >= 309 && y <= 441)
+                DrawButton(900, 308, 1020, 441, True);
+            else if (y >= 442 && y <= 574)
+                DrawButton(900, 441, 1020, 574, True);
+            else if (y >= 575 && y <= 707)
+                DrawButton(900, 574, 1020, 707, True);
+
     } while (True);
+    /* closegraph(); */
+}
+
+/* Funcion que Guarda en un Fichero una Accion */
+void SaveStep(void)
+{
+    Iteracion i = 0, j = 0;
+
+    DataBase = fopen(DataBasePath, "a+");
+
+    if (DataBase == NULL)
+    {
+        /* Have No Idea!! */
+    } else {
+        for (i = 0; i < 200; i++)
+            for (j = 0; j < 300; j++)
+                fprintf(DataBase, "%d\n", getpixel(j,i));
+        exit(0);
+    }
+}
+
+typedef struct DataTable {
+    ControlEje x1, y1, x2, y2;
+    unsigned short Pixel;
+} DataBaseConexion;
+
+void CalcAmountOfMemory(void)
+{
+
+}
+
+struct DataTable *MemoryConstructor(Cantidad RegistrosSolicitados)
+{
+    /* Variable de tipo Puntero Para Acceder a la Estructura */
+    struct DataTable *Memoria_De_Registros = NULL;
+
+    /* Calculando el espacio de Memoria Solicitado */
+    Cantidad MemoriaRequerida = (sizeof(struct DataTable) * RegistrosSolicitados);
+
+    /* Asignacion de Memoria a la Estructura */
+    Memoria_De_Registros = (struct DataTable *) malloc(MemoriaRequerida);
+
+    /* Si Se Logro Asignar la Memoria */
+    if (Memoria_De_Registros != NULL)
+    {
+        memset(&Memoria_De_Registros, '', TamArray(Memoria_De_Registros));
+    } else {
+        /* No Idea!! about what to do! */
+    }
+
+    return Memoria_De_Registros;
 }
